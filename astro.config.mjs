0efc0be49ad5cc6e4956b7c1e4ecca8e289c5329
gsgr789 <z58m-92w@proton.me>
2024-02-29 17:04:00 -0700

    added config changes to deploy in github

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,12 @@ import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
-  site: 'https://astro-blog-template.netlify.app',
+  //the next two lines is so that astro website can be build in github pages
+  site: 'https://gsgr789.github.io',
+  base: '/blog',
+
+  //adapter: netlify(),
+  //site: 'https://astro-blog-template.netlify.app',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {

be51548cb8c6aab0178588c4828ccfc35c70696e
gsgr789 <z58m-92w@proton.me>
2024-02-29 17:59:37 -0700

    changed base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,9 @@ import rehypeExternalLinks from 'rehype-external-links'
 export default defineConfig({
   //the next two lines is so that astro website can be build in github pages
   site: 'https://gsgr789.github.io',
-  base: '/blog',
+  base: '',
 
-  //adapter: netlify(),
+  adapter: netlify(),
   //site: 'https://astro-blog-template.netlify.app',
   integrations: [mdx(), svelte()],
   markdown: {
